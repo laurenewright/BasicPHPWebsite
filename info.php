cffb87c7651bc1080acecf8d8591a2e0910e1d4f
lauren wright <wright.lauren20@gmail.com>
2019-04-20 16:46:16 -0400

    updated sprites

---
+++ info.php
@@ -0,0 +1,13 @@
+<?php
+    $pageTitle = "Company Information";
+    $section = "info";
+
+    include("include/header.php"); 
+?>
+
+<div class="section page">
+    <h1><?php echo $pageTitle; ?></h1>
+</div>
+
+
+<?php include("include/footer.php"); ?>
